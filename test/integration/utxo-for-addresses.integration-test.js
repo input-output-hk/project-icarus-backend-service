cefb90812d3b6b11971b2af101d6b2dc660cf215
Alan Verbner <alan.verbner@iohk.io>
2018-06-05 11:48:18 -0300

    [PI-123] More tests and files renaming

--- test/integration/utxo-for-addresses.integration-test.js
+++ test/integration/utxo-for-addresses.integration-test.js
@@ -1,6 +1,7 @@
 // @flow
 const shuffle = require('shuffle-array');
-const { runInServer } = require('./test-utils');
+const { expect } = require('chai');
+const { runInServer, assertOnResults } = require('./test-utils');
 
 const ENDPOINT = '/txs/utxoForAddresses';
 
@@ -48,6 +49,7 @@ describe('UtxoForAddresses endpoint', () => {
   it('should filter unused addresses', async () => {
     const usedAddresses = [
       'DdzFFzCqrhshvqw9GrHmSw6ySwViBj5cj2njWj5mbnLu4uNauJCKuXhHS3wNUoGRNBGGTkyTFDQNrUWMumZ3mxarAjoXiYvyhead7yKQ',
+      'DdzFFzCqrhskrzzPrXynkZ3gteGy8GmWYrswqz9SueoFP9PV5suFnGv9sQqg3o5pxzFpDTJ2HFJzHrThxBYarQi8guzMUhuiePB1T6ff',
     ];
 
     const unusedAddresses = [
@@ -56,23 +58,39 @@ describe('UtxoForAddresses endpoint', () => {
       'DdzFFzCqrht8d5FeU62PpBw1e3JLUP48LKfDfNtUyfuBJjBEqmgfYpwcbNHCh3csA4DEzu7SYquoUdmkcknR1E1D6zz5byvpMx632VJx',
     ];
 
+    const expectedUTOXs = [
+      {
+        utxo_id:
+          '6cc6d736e3a4395acabfae4c7cfe409b65d8c7c6bbf9ff85a0bd4a95334b7a5f0',
+        tx_hash:
+          '6cc6d736e3a4395acabfae4c7cfe409b65d8c7c6bbf9ff85a0bd4a95334b7a5f',
+        tx_index: 0,
+        receiver:
+          'DdzFFzCqrhshvqw9GrHmSw6ySwViBj5cj2njWj5mbnLu4uNauJCKuXhHS3wNUoGRNBGGTkyTFDQNrUWMumZ3mxarAjoXiYvyhead7yKQ',
+        amount: '1463071700828754',
+      },
+      {
+        utxo_id:
+          'aba9ad6b8360542698038dea31ca23037ad933c057abc18c5c17c2c63dbc3d131',
+        tx_hash:
+          'aba9ad6b8360542698038dea31ca23037ad933c057abc18c5c17c2c63dbc3d13',
+        tx_index: 1,
+        receiver:
+          'DdzFFzCqrhskrzzPrXynkZ3gteGy8GmWYrswqz9SueoFP9PV5suFnGv9sQqg3o5pxzFpDTJ2HFJzHrThxBYarQi8guzMUhuiePB1T6ff',
+        amount: '9829100',
+      },
+    ];
+
     const addresses = shuffle(usedAddresses.concat(unusedAddresses));
     return runInServer(api =>
       api
         .post(ENDPOINT)
         .send({ addresses })
-        .expectBody([
-          {
-            utxo_id:
-              '6cc6d736e3a4395acabfae4c7cfe409b65d8c7c6bbf9ff85a0bd4a95334b7a5f0',
-            tx_hash:
-              '6cc6d736e3a4395acabfae4c7cfe409b65d8c7c6bbf9ff85a0bd4a95334b7a5f',
-            tx_index: 0,
-            receiver:
-              'DdzFFzCqrhshvqw9GrHmSw6ySwViBj5cj2njWj5mbnLu4uNauJCKuXhHS3wNUoGRNBGGTkyTFDQNrUWMumZ3mxarAjoXiYvyhead7yKQ',
-            amount: '1463071700828754',
-          },
-        ])
+        .expect(
+          assertOnResults((res, body) => {
+            expect(body).to.have.same.deep.members(expectedUTOXs);
+          }),
+        )
         .end(),
     );
   });
