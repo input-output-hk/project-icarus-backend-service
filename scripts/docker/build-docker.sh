04469543d99aeaea08ef35290075cbf92b2be79b
Alan Verbner <alan.verbner@iohk.io>
2018-06-07 22:12:38 -0300

    Configure node to start as HTTPS using self singed certs

--- scripts/docker/build-docker.sh
+++ scripts/docker/build-docker.sh
@@ -7,6 +7,4 @@ source "$MY_DIR/helper.sh";
 PACKAGE_VERSION=$(readPackageJSON 'version');
 NAME=$(readPackageJSON 'name');
 
-flow-remove-types ./src/ -d ./flow-files/ --all --pretty;
-
 docker build -t "icarus/$NAME:$PACKAGE_VERSION" -f ./docker/Dockerfile .
