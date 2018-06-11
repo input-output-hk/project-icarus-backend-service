c932f8610d5674d6af6b087231577e88796eb940
github_iohk.com <mirko.alic@iohk.io>
2018-06-11 12:13:25 -0300

    [PI-166] Clean code

--- src/ws-connections.js
+++ src/ws-connections.js
@@ -9,32 +9,39 @@ const Lazy = require('lazy.js');
 const fromMessage: any = JSON.parse;
 const toMessage = JSON.stringify;
 
-// TODO: Move to config
 const MSG_TYPE_RESTORE = 'RESTORE';
-const ADDRESSES_CHUNK_SIZE = 250000;
+// FIXME: Manage the case of more than one step.
+const ADDRESSES_CHUNK_SIZE = 1000000000;
+
+function handleRestore(
+  dbApi: DbApi,
+  { logger }: LoggerObject,
+  ws: any,
+): void {
+  dbApi.unspentAddresses().then(result => {
+    logger.debug(`[WS::onMessage] ${MSG_TYPE_RESTORE} - Db result ready`);
+    const addresses = Lazy(result.rows);
+    logger.debug(`[WS::onMessage] ${MSG_TYPE_RESTORE} - Addresses processing start`);
+    addresses
+      .flatten()
+      .chunk(ADDRESSES_CHUNK_SIZE)
+      .each((chunk, index) => {
+        logger.debug(`[WS::onMessage] ${MSG_TYPE_RESTORE} - Addresses processing step ${index}`);
+        ws.send(toMessage({
+          msg: MSG_TYPE_RESTORE,
+          addresses: chunk,
+        }));
+      });
+  });
+}
 
 module.exports = (dbApi: DbApi, { logger }: LoggerObject) => (ws: any) => {
   ws.on('message', (msg) => {
-    logger.debug('[WS::onMessage] %s', msg);
+    logger.debug(`[WS::onMessage] ${msg}`);
     const data = fromMessage(msg);
     switch (data.msg) {
       case MSG_TYPE_RESTORE:
-        dbApi.unspentAddresses().then(result => {
-          logger.debug(`[WS::onMessage] ${MSG_TYPE_RESTORE} - db result ready`);
-          const addresses = Lazy(result.rows);
-          logger.debug(`[WS::onMessage] ${MSG_TYPE_RESTORE} - addresses processing start`);
-          addresses
-            .flatten()
-            .chunk(ADDRESSES_CHUNK_SIZE)
-            .each((chunk, index) => {
-              logger.debug(`[WS::onMessage] ${MSG_TYPE_RESTORE} - addresses processing step ${index}`);
-              ws.send(toMessage({
-                msg: 'RESTORE',
-                step: index,
-                addresses: chunk,
-              }));
-            });
-        });
+        handleRestore(dbApi, { logger }, ws);
         break;
       default:
         break;
