0352131fa41168e3d5f380c98406bf6710195942
Alan Verbner <alan.verbner@iohk.io>
2018-06-19 10:28:34 -0300

    Service can now be ran in AWS lambda

--- src/db.js
+++ src/db.js
@@ -4,9 +4,4 @@ import type { PgPoolConfig, Pool } from 'pg';
 
 const PG = require('pg');
 
-module.exports = async (dbSettings: PgPoolConfig): Promise<Pool> => {
-  const pool = new PG.Pool(dbSettings);
-  // Test connection
-  await pool.query('SELECT NOW()');
-  return pool;
-};
+module.exports = (dbSettings: PgPoolConfig): Pool => new PG.Pool(dbSettings);
