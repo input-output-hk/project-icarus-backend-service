d06e762b80a065c2bd48e4a0483847fc12553c1f
Gaël Deest <gael.deest@tweag.io>
2018-08-27 13:11:03 +0200

    Explictly load environment variables from deploy.js

--- config/deploy.js
+++ config/deploy.js
@@ -1,18 +1,25 @@
 const { raw } = require('config/raw');
 const { consoleLogger } = require('../src/logger');
 
+var getEnv = function(name) {
+    var ret = process.env[name];
+    if (!ret)
+        throw ("Environment variables should be defined: " + name);
+    return ret;
+}
+
 module.exports = {
     server: {
         corsEnabledFor: ['*'],
-        logger: raw(consoleLogger('error')),
+        logger: raw(consoleLogger('info')),
         port: 8080,
-        importerSendTxEndpoint: 'fake',
+        importerSendTxEndpoint: getEnv("IMPORTER_ENDPOINT"),
     },
     db: {
-        user: 'fake',
-        host: 'fake',
-        database: 'fake',
-        password: 'fake',
+        user: getEnv("DB_USER"),
+        host: getEnv("DB_HOST"),
+        database: getEnv("DB"),
+        password: getEnv("DB_PASSWORD"),
         port: '5432',
         min: 4,
         max: 50,
