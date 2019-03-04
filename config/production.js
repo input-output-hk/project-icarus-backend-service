eb48768faa31c99680c30567899009597af741ec
refi93 <rafael.korbas@gmail.com>
2019-03-04 23:21:36 +0100

    add adalite staging 2 to allowed origins

--- config/production.js
+++ config/production.js
@@ -5,7 +5,7 @@ module.exports = {
   appName: 'icarus-poc-backend-service',
   server: {
     corsEnabledFor: ['https://adalite.io', 'https://*.adalite.io', 'http://*.adalite.io',
-     'https://adalite-staging.herokuapp.com', 'http://localhost:*', 'https://localhost:*'],
+     'https://adalite-staging.herokuapp.com', 'https://adalite-staging2.herokuapp.com', 'http://localhost:*', 'https://localhost:*'],
     allowCredentials: true,
     logger: raw(consoleLogger('error')),
     port: 8080,
